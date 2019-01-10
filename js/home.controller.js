9f55ccad56a105b5842aacb24754c9f4df22bd8b
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2019-01-10 13:54:18 -0600

    Probando scaneo

--- js/home.controller.js
+++ js/home.controller.js
@@ -63,12 +63,29 @@ function callHome(){
 	$("#btnScanIMEI").click(function(){
 		cordova.plugins.barcodeScanner.scan(
 			function (result) {
-				alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);
+				$("#txtIMEI").val(result.text);
 			}, function (error) {
-				alert("Scanning failed: " + error);
+				mensajes.log({"mensaje": "Scaneo cancelado"});
+			}, {
+				preferFrontCamera : true, // iOS and Android
+				showFlipCameraButton : true, // iOS and Android
+				showTorchButton : true, // iOS and Android
+				torchOn: true, // Android, launch with the torch switched on (if available)
+				saveHistory: true, // Android, save scan history (default false)
+				prompt : "Scanea el IMEI", // Android
+				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+				disableAnimations : true, // iOS
+				disableSuccessBeep: false // iOS and Android
+			});
+	});
+	$("#btnScanICCID").click(function(){
+		cordova.plugins.barcodeScanner.scan(
+			function (result) {
+				$("#txtICCID").val(result.text);
+			}, function (error) {
+				mensajes.log({"mensaje": "Scaneo cancelado"});
 			}, {
 				preferFrontCamera : true, // iOS and Android
 				showFlipCameraButton : true, // iOS and Android
