1c6626f75f6eac5ad6cbae60624d9c234968f3b8
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2019-01-15 17:20:02 -0600

    Cambio de orientación de la cámara

--- js/home.controller.js
+++ js/home.controller.js
@@ -112,7 +112,7 @@ function callHome(){
 				prompt : "Scanea el IMEI", // Android
 				resultDisplayDuration: 50, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
 				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-				orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+				orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 				disableAnimations : true, // iOS
 				disableSuccessBeep: false // iOS and Android
 			});
@@ -136,7 +136,7 @@ function callHome(){
 				prompt : "Scanea el IMEI", // Android
 				resultDisplayDuration: 50, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
 				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-				orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+				orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 				disableAnimations : true, // iOS
 				disableSuccessBeep: false // iOS and Android
 			});
